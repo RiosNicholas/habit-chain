b0537450678444f079abaac5e8327b63fcd522f7
Nicholas <85625773+RiosNicholas@users.noreply.github.com>
2023-03-20 21:38:12 -0400

    Update button padding and add habit data

--- HabitChain/HabitChain/ContentView.swift
+++ HabitChain/HabitChain/ContentView.swift
@@ -18,23 +18,34 @@ struct ContentView: View {
                 .padding(.bottom, 20.0)
         
             // HStacks hold two columns of buttons 
-            HStack(spacing: 50){
-                HabitButton(habit: "Drink Water", icon: "💧", backgroundColor: .green, textColor: .white)
-                HabitButton(habit: "Drink Water", icon: "💧", backgroundColor: .green, textColor: .white)
+            
+             HStack(spacing: 0){
+                HabitButton(habit: "Late Bedtime", emoji: "😴", goodHabit: false, textColor: .black, gradientColor: [.red, .white])
+                
+                HabitButton(habit: "Sugary Foods", emoji: "🍩", goodHabit: false, textColor: .black, gradientColor: [.red, .white])
+                //HabitButton(habit: "Drink Water", icon: "💧", backgroundColor: .green, textColor: .white)
             }
             
-            HStack(spacing: 50){
-                HabitButton(habit: "Drink Water", icon: "💧", backgroundColor: .green, textColor: .white)
-                HabitButton(habit: "Drink Water", icon: "💧", backgroundColor: .green, textColor: .white)
+            
+            HStack(spacing: 0){
+                HabitButton(habit: "Reading", emoji: "📖", goodHabit: true, textColor: .white, gradientColor: [.green, .white])
+                HabitButton(habit: "Journaling", emoji: "📝", goodHabit: false, textColor: .white, gradientColor: [.green, .white])
             }
             
             
-            HStack(spacing: 50){
-                HabitButton(habit: "Drink Water", icon: "💧", backgroundColor: .green, textColor: .white)
-                    .padding(0)
-                HabitButton(habit: "Drink Water", icon: "💧", backgroundColor: .green, textColor: .white)
+            HStack(spacing: 0){
+                HabitButton(habit: "Stretching", emoji: "🙆‍♂️", goodHabit: true, textColor: .white, gradientColor: [.green, .white])
+                HabitButton(habit: "Exercise", emoji: "🏋️", goodHabit: true, textColor: .white, gradientColor: [.green, .white])
             }
+            /*
             
+             //I give up on this for now, but I would have wanted to use a datamodel to input my data
+             List {
+                ForEach(DataModel.habits, id: \.self) { habitData in
+                    HabitButton(habit: habitData.habit, emoji: habitData.emoji, goodHabit: habitData.goodHabit, textColor: habitData.textColor, gradientColor: habitData.gradientColor)
+                }
+            }
+             */
             Spacer() // Removes white space and pushes content to fill the entire screen
         }
         .background(Color(.systemGray2).edgesIgnoringSafeArea(.all)) // Setting the app's background
