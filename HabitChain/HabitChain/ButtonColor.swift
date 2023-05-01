bb9168aa80f6a23ff7d5567def7e337446ae6e20
Nicholas <85625773+RiosNicholas@users.noreply.github.com>
2023-05-01 13:52:31 -0400

    Update Habit Button layout and sizing
    - Create fixed size for all HabitButtons with responsive text sizing
    - Replace nested HStacks with responsive grid using LazyVGrid

--- HabitChain/HabitChain/ButtonColor.swift
+++ HabitChain/HabitChain/ButtonColor.swift
@@ -1,20 +1,54 @@
+////
+////  ButtonColor.swift
+////  HabitChain
+////
+////  Created by Nicholas Rios on 3/20/23.
+////
 //
-//  ButtonColor.swift
-//  HabitChain
+//import SwiftUI
 //
-//  Created by Nicholas Rios on 3/20/23.
+//enum ButtonColor {
+//    case good(chainNum: Int, gradientColor: [Color])
+//    case bad(chainNum: Int, gradientColor: [Color])
+//    case zero
+//    
+//    var gradient: [Color] {
+//        switch self {
+//        case .zero:
+//            return [.white]
+//        case .good(let chainNum, _):
+//            let greenDepth = min(Double(chainNum) / 100, 1.0)
+//            return [Color(red: 0.0, green: greenDepth, blue: 0.0), .white]
+//        case .bad(let chainNum, _):
+//            let redDepth = min(Double(chainNum) / 100, 1.0)
+//            return [Color(red: redDepth, green: 0.0, blue: 0.0), .white]
+//        }
+//    }
+//    
+//    var textColor: Color {
+//        switch self {
+//        case .good:
+//            return .white
+//        default:
+//            return .black
+//        }
+//    }
+//    
+//    init(chainNum: Int, gradientColor: [Color], goodHabit: Bool) {
+//        if chainNum == 0 {
+//            self = .zero
+//        } else if goodHabit {
+//            self = .good(chainNum: chainNum, gradientColor: gradientColor)
+//        } else {
+//            self = .bad(chainNum: chainNum, gradientColor: gradientColor)
+//        }
+//    }
+//}
 //
-
-import SwiftUI
-
-struct ButtonColor: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ButtonColor_Previews: PreviewProvider {
-    static var previews: some View {
-        ButtonColor()
-    }
-}
+//
+//
+//struct ButtonColor_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ButtonColor.good(chainNum: 59, gradientColor: [.green, .white])
+//    }
+//}
